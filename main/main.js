84a904cb357ef92e3f9fb756cb90be5690ca2f2a
Zamzam93 <samira0101@live.com>
2022-09-15 12:21:30 -0500

    did final clean up

--- main/main.js
+++ main/main.js
@@ -6,16 +6,13 @@ const notesCallback = ({ data: notesDB }) => displayNotes(notesDB)
 
 
 
-//make function that looks like #6
-// function submitHandler(e){
-//     e.preventDefault();
-//     console.log("hello, world")
+
 
     
 let noteContainer = document.querySelector('.notes__preview')
 function createNoteCards(notes){
     noteList.innerHTML = "<div>"
-    //do a for loop to loop through and create note for each note
+    
     for (let i = 0; i < notes.length ; i++) {
         let note = notes[i]
         let noteCard = document.createElement('div')
@@ -24,12 +21,6 @@ function createNoteCards(notes){
     }
     noteList.innerHTML += "</div>"
 
-    /*
-
-        Problem is we stack cards in a single Notelist. 
-        When a card is clicked for deletion, every card gets deleted.
-        We need to seperate the cards out into their own elements
-    */
 }
 
 
@@ -70,12 +61,7 @@ function addNote(){
        console.log(res.data)
        createNoteCards(res.data)
    })
-    // let noteTitle = document.querySelector(".notes__small-tittle")
     
-    // let noteBody = document.querySelector(".notes__small-body")
-    // const note = document.createElement("div")
-
-    // note.textContent = noteTitle.value;
 }
 
 function displayNotes(arr){
@@ -85,8 +71,6 @@ function displayNotes(arr){
     }
 }
 
-//get post to work hopefully by tomorrow.
 
-//once addnote works note then in the createnote function put in the hmtl function for delete button
 addNoteButton.addEventListener('click', addNote)
 window.addEventListener('load', getAllNotes)
